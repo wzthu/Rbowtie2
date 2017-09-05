b9984d2cff46db61c1332673e491311e76e40265
wzthu <wzweizheng@qq.com>
2017-09-05 16:07:03 +0800

    fix shared library required for Mac

---
+++ src/version_info.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+//using namespace std;
+
+void getInfo(){
+    //cout<<"The version of AdapterRemoval is 2.2.1a"<<endl;
+    //cout<<"The version of Bowtie2 is 2.3.2"<<endl;
+    //cout<<"They are modified to be portable for Windows and Mac OS"<<endl;
+}
