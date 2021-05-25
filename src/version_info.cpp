c851d9c16b14f80b787c3e3d437e68659760934d
Rahul Varki <rahulvarki@varkis-MacBook-Pro.local>
2021-05-25 16:26:30 -0400

    Added large bowtie index version

--- src/version_info.cpp
+++ src/version_info.cpp
@@ -5,4 +5,5 @@ void getInfo(){
     //cout<<"The version of AdapterRemoval is 2.2.1a"<<endl;
     //cout<<"The version of Bowtie2 is 2.3.2"<<endl;
     //cout<<"They are modified to be portable for Windows and Mac OS"<<endl;
+    //cout<<"bowtie2-build-l and bowtie2-align-l are version 2.4.4"<<endl;
 }
