591d7a165491943e303fd3fd5c322de05d4f1883
Rahul Varki <rahulvarki@varkis-MacBook-Pro.local>
2021-05-26 20:52:31 -0400

    Fixed Typo

--- R/bowtie2.R
+++ R/bowtie2.R
@@ -108,7 +108,7 @@ bowtie2 <- function(bt2Index,indexFormat = 's',samOutput,seq1,...,seq2=NULL,inte
         checkFileExist(paste0(bt2Index,".1.bt2"),"bt2Index")
         checkFileExist(paste0(bt2Index,".2.bt2"),"bt2Index")
         checkFileExist(paste0(bt2Index,".3.bt2"),"bt2Index")
-        checkFileExist(paste0(bt2Index,".4.bt2"),"bt2ndex")
+        checkFileExist(paste0(bt2Index,".4.bt2"),"bt2Index")
         checkFileExist(paste0(bt2Index,".rev.1.bt2"),"bt2Index")
         checkFileExist(paste0(bt2Index,".rev.2.bt2"),"bt2Index")
     }
