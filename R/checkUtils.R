3d5addd12b2b38a020b1025f314e835c6d8bf5ce
Rahul Varki <rahulvarki@varkis-MacBook-Pro.local>
2021-08-25 13:56:14 -0400

    Added function to check for samtools

--- R/checkUtils.R
+++ R/checkUtils.R
@@ -1,22 +1,17 @@
 #'@name checkIndexType
-#'
 #'@title Determine bowtie2 index type
-#'
 #'@description This is an internal function that is not meant to be used outside
 #'of the package. It determines whether the given index library comprises of 
-#'small indexes (.bt2) or large indexes (.bt2l)
-#'
+#'small indexes (.bt2) or large indexes (.bt2l).
 #'@param filePath \code{Character} scalar. The path where bowtie2 index files 
 #'are located. Include the basename of the index files at the end of the path
 #'(i.e. path_to_index_dir/index_basename). Do not include the bowtie2 index file 
-#'extension (.bt2 or .bt2l)
-#'
+#'extension (.bt2 or .bt2l).
 #'@details The function first tries to determine whether 
 #'path_to_index_dir/index_basename.1.bt2 exists and if it doesn't exist then it
 #'tries to determine whether path_to_index_dir/index_basename.1.bt2l exists. 
 #'If neither of those files exist then there is an issue with the index files 
 #'that must be addressed.
-#'
 #'@return \code{Character} scalar. Returns either "SMALL" if the .bt2 file is found,
 #'"LARGE" if the .bt2l file is found, or "ERROR" if neither the .bt2 nor .bt2l file 
 #'is found.
@@ -36,19 +31,13 @@ checkIndexType <- function(filePath){
 } 
 
 #'@name checkFileExist
-#'
 #'@title Check if file exists 
-#'
 #'@description This is an internal function that is not meant to be used outside
 #'of the package. It determines whether a specific file exists at the end of the
 #'path given to the function.
-#'
 #'@param filePath \code{Character} scalar. Path to file of interest.
-#'
 #'@param argname \code{Character} scalar. Name of the argument passed.
-#'
 #'@author Zheng Wei
-#'
 #'@return Stops the function if the file does not exist.
 
 
@@ -65,18 +54,12 @@ checkFileExist <- function(filePath,argname){
 
 
 #'@name checkPathExist
-#'
 #'@title Check if path exists 
-#'
 #'@description This is an internal function that is not meant to be used outside
 #'of the package. It determines whether the path passed to the function exists.
-#'
 #'@param filePath \code{Character} scalar. Path of interest.
-#'
 #'@param argname \code{Character} scalar. Name of the argument passed.
-#'
 #'@author Zheng Wei
-#'
 #'@return Stops the function if the path does not exist.
 
 
@@ -90,21 +73,14 @@ checkPathExist <- function(filePath,argname){
 }
 
 #'@name checkFileCreateable
-#'
 #'@title Check if file is creatable
-#'
 #'@description This is an internal function that is not meant to be used outside
 #'of the package. It determines whether the file at the end of the path can
 #'be created. 
-#'
 #'@param filePath \code{Character} scalar. Path to file to be created.
-#'
 #'@param argname \code{Character} scalar. Name of the argument passed.
-#'
 #'@param overwrite \code{Logical} Will warn user of overwriting if set \code{TRUE}.
-#'
 #'@author Zheng Wei
-#'
 #'@return Indicates whether the file can be created at the path given.
 
 
@@ -126,6 +102,22 @@ checkFileCreatable <- function(filePath,argname,overwrite){
         }
     }
 }
+
+#'@name checkSamtoolsExists
+#'@title Check if samtools exists on the system
+#'@description This is an internal function that is not meant to be used outside
+#'of the package. It checks whether samtools exists on the system.
+#'@return Returns TRUE if samtools exists on the system, else FALSE
+
+checkSamtoolsExists <- function(){
+    if (file.exists(Sys.which("samtools")))
+        return(TRUE)
+    else
+        return(FALSE)
+}
+
+
+
 
 checkAddArgus<- function(pattern,...){
     paramlist<-trimws(as.character(list(...)))
